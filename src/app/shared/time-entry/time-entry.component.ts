33aaed56a29042c0d320ebe758622b6bcd18ad41
Ryan McCormick <ryan@itjobsguy.com>
2017-10-29 23:54:42 -0500

    add two way binding for time-entry component

--- src/app/shared/time-entry/time-entry.component.ts
+++ src/app/shared/time-entry/time-entry.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
+
 
 /**
  * Time Entry Component: Allows a user to input time
@@ -9,70 +11,107 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './time-entry.component.html',
   styleUrls: ['./time-entry.component.scss']
 })
-export class TimeEntryComponent implements OnInit {
+export class TimeEntryComponent implements ControlValueAccessor, OnInit {
 
-  /**
-   * Time Hour Selected (1 - 12).
-   */
-  selectedHours: number;
-
-  /**
-   * Time Minute Selected (00 - 59).
-   */
-  selectedMin: number;
-
-  /**
-   * Time of Day Selected (AM, PM).
-   */
-  selectedTod: string;
-
-  /**
-   * Display Selection Field Labels.
-   */
   showLabels: boolean;
-
-  /**
-   * Optional: Default hour selection.
-   */
+  @Output() timeChanged = new EventEmitter();
+  @Output() timeChange = new EventEmitter();
   @Input() defaultHr?: number;
-
-  /**
-   * Optional: Default minute selection.
-   */
   @Input() defaultMin?: number;
-
-  /**
-   * Optional: Default time of day selection.
-   */
   @Input() defaultTod?: string;
-
-  /**
-   * Optional: Display field labels.
-   */
   @Input() labels?: boolean;
+  private _time: Date;
 
-  /**
-   * Time Entry Component: Allows a user to input time
-   * of day based on hour, minute and am/pm.
-   */
   constructor() { }
 
-  /**
-   * Angular component lifecycle event.
-   * Enforced by interface OnInit.
-   */
   ngOnInit() {
-    this.setDefaults();
   }
 
-  /**
-   * Set default values based on optional input params.
-   */
-  setDefaults(): void {
-    this.selectedHours = this.defaultHr ? this.defaultHr : 1;
-    this.selectedMin = this.defaultMin ? this.defaultMin : 0;
-    this.selectedTod = this.defaultTod ? this.defaultTod : 'am';
-    this.showLabels = this.labels ? this.labels : false;
+  @Input()
+  set time(time: Date) {
+    this.setTime(time);
+  }
+
+  setTime(time: Date) {
+    this._time = time;
+  }
+
+  get selectedHours(): number {
+    const timeOfDayModifier = this.isMorning ? 0 : 12;
+    const currentHours = this._time.getHours();
+    const calculatedHour = Math.abs(currentHours - timeOfDayModifier);
+
+    return calculatedHour ? calculatedHour : 12;
+  }
+
+  set selectedHours(value: number) {
+    const timeOfDayModifier = this.isMorning ? 0 : 12;
+    const selectedHour = parseInt(value.toString(), 10);
+    const calculatedHour = selectedHour + timeOfDayModifier;
+
+    this._time.setHours(calculatedHour);
+    this.updateTime();
+  }
+
+  get selectedMin(): number {
+    const selectedMin = this._time.getMinutes();
+    return selectedMin;
+  }
+
+  set selectedMin(value: number) {
+    const minutes = parseInt(value.toString(), 10);
+
+    this._time.setMinutes(minutes);
+    this.updateTime();
+  }
+
+  get selectedTod(): string {
+    return this.isMorning ? 'am' : 'pm';
+  }
+
+  set selectedTod(value: string) {
+    const isMorning = (value === 'am');
+    const currentHour = this._time.getHours();
+    const calculatedHour = isMorning ? currentHour + 12 : currentHour - 12;
+
+    this._time.setHours(calculatedHour);
+    this.updateTime();
+  }
+
+  writeValue(value: any): void {
+    this.setTime(value);
+  }
+
+  private _onChange = (_: any) => {
+  }
+
+  registerOnChange(fn: any): void {
+    this._onChange = fn;
+  }
+
+  private _onTouched = () => {
+  }
+
+  registerOnTouched(fn: any): void {
+    this._onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+  }
+
+  get time() {
+    return this._time;
+  }
+
+  get isMorning(): boolean {
+    const currentHour = this._time.getHours();
+    return currentHour >= 0 && currentHour < 12;
+  }
+
+  private updateTime() {
+    const newTime = new Date(this._time);
+    this.timeChange.emit(newTime);
+    this.timeChanged.emit(newTime);
   }
 
 }
