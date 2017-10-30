33d3fce214e19f46b14027bbe9ae7a50cf21d1ce
Ryan McCormick <ryan@itjobsguy.com>
2017-10-30 00:35:24 -0500

    add two way binding

--- src/app/shared/time-block-entry/time-block.model.ts
+++ src/app/shared/time-block-entry/time-block.model.ts
@@ -1,4 +1,9 @@
 export class TimeBlock {
   hours: number;
   minutes: number;
+
+  constructor(hours?: number, minutes?: number) {
+    this.hours = hours || 0;
+    this.minutes = minutes || 0;
+  }
 }
