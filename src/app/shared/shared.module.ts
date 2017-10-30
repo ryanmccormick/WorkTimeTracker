dffbd5b187143d50153289740fcaa8fb2b51c675
Ryan McCormick <ryan@itjobsguy.com>
2017-10-29 22:06:00 -0500

    add documentation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { TimeEntryComponent } from './time-entry/time-entry.component';
 import { TimeBlockEntryComponent } from './time-block-entry/time-block-entry.component';
-import { FormsModule } from '@angular/forms';
 
 export const sharedComponents = [TimeEntryComponent, TimeBlockEntryComponent];
 
+/**
+ * SharedModule: Exports and declares components to be shared across
+ * the application. This module must be imported into the parent module
+ * of components that need access to shared components.
+ */
 @NgModule({
   imports: [
     CommonModule,
--- src/app/shared/time-entry/time-entry.component.ts
+++ src/app/shared/time-entry/time-entry.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+/**
+ * Time Entry Component: Allows a user to input time
+ * of day based on hour, minute and am/pm.
+ */
 @Component({
   selector: 'app-time-entry',
   templateUrl: './time-entry.component.html',
@@ -7,22 +11,63 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TimeEntryComponent implements OnInit {
 
+  /**
+   * Time Hour Selected (1 - 12).
+   */
   selectedHours: number;
+
+  /**
+   * Time Minute Selected (00 - 59).
+   */
   selectedMin: number;
+
+  /**
+   * Time of Day Selected (AM, PM).
+   */
   selectedTod: string;
+
+  /**
+   * Display Selection Field Labels.
+   */
   showLabels: boolean;
 
+  /**
+   * Optional: Default hour selection.
+   */
   @Input() defaultHr?: number;
+
+  /**
+   * Optional: Default minute selection.
+   */
   @Input() defaultMin?: number;
+
+  /**
+   * Optional: Default time of day selection.
+   */
   @Input() defaultTod?: string;
+
+  /**
+   * Optional: Display field labels.
+   */
   @Input() labels?: boolean;
 
+  /**
+   * Time Entry Component: Allows a user to input time
+   * of day based on hour, minute and am/pm.
+   */
   constructor() { }
 
+  /**
+   * Angular component lifecycle event.
+   * Enforced by interface OnInit.
+   */
   ngOnInit() {
     this.setDefaults();
   }
 
+  /**
+   * Set default values based on optional input params.
+   */
   setDefaults(): void {
     this.selectedHours = this.defaultHr ? this.defaultHr : 1;
     this.selectedMin = this.defaultMin ? this.defaultMin : 0;
